40f88a3505d5c4f74df79953c9a7e092bc488aef
fabioyk <kyongxd@gmail.com>
2017-06-02 20:52:18 -0300

    fixing validation

--- server/validation.js
+++ server/validation.js
@@ -190,7 +190,8 @@ module.exports = {
   checkCharacters(stringCheck, alphabet) {    
     for (var i=0; i<stringCheck.length; i++) {
       if (alphabet.indexOf(stringCheck.charAt(i)) === -1) {
-        console.log('VALIDATION ERROR:',stringCheck,'Not from alphabet',alphabet);
+        console.log('VALIDATION ERROR: '+stringCheck+' Not from alphabet '+alphabet);
+        console.log('At position '+i+' - '+stringCheck.chartAt(i));
         return false;
       }
     }
