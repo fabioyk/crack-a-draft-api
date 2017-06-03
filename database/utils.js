5d0db58da3f0bebeb85e8a6a892c2c5ec2ea9373
fabioyk <kyongxd@gmail.com>
2017-06-03 20:41:17 -0300

    fixed utils functions for submit crack

--- database/utils.js
+++ database/utils.js
@@ -27,5 +27,31 @@ module.exports = {
     return function(result) {
       callback(null, result);
     }
-  }
+  },
+
+  convertBase(src,srctable,desttable) {
+    var srclen=srctable.length;
+    var destlen=desttable.length;
+    var val=0;
+    var numlen=src.length;
+    for(var i=0;i<numlen;i++) {
+      var ind = srctable.indexOf(src.charAt(i));
+      if (ind === -1){
+        ind = 0;}
+      val=val*srclen+ind;
+    }
+    console.log(val)
+    if(val<0) {
+      return 0;
+    }
+    var r=val%destlen;
+    var res=desttable.charAt(r);
+    var q=Math.floor(val/destlen);
+    while(q) {
+      r=q%destlen;
+      q=Math.floor(q/destlen);
+      res=desttable.charAt(r)+ res;
+    }
+    return res;
+  },
 }
--- server/utils.js
+++ server/utils.js
@@ -1,35 +1,4 @@
 var utils = {
-  
-  convertBase(src,srctable,desttable) {
-    var srclen=srctable.length;
-    var destlen=desttable.length;
-    var val=0;
-    var numlen=src.length;
-    for(var i=0;i<numlen;i++) {
-      var ind = srctable.indexOf(src.charAt(i));
-      if (ind === -1){
-        ind = 0;}
-      val=val*srclen+ind;
-    }
-    console.log(val)
-    if(val<0) {
-      return 0;
-    }
-    var r=val%destlen;
-    var res=desttable.charAt(r);
-    var q=Math.floor(val/destlen);
-    while(q) {
-      r=q%destlen;
-      q=Math.floor(q/destlen);
-      res=desttable.charAt(r)+ res;
-    }
-    return res;
-  },
-  
-  formatDate(stringDate) {
-    
-  },
-
   treatResult(res, err, obj) {
     if (err) {
       console.log(err);
